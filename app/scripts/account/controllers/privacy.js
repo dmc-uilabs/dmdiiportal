1d23de9be596dcd6c8ac61c3f9b1b151ace609de
Denis <klassik911@ukr.net>
2015-12-11 11:57:04 +0200

    Names shouldn't cut off in blog posts
    Create privacy section of manage account page
    - When selecting a checkbox, the relevant field appears below allows the user to enter in information. Instead of them always being there & grayed out, they should be hidden until selected.
    - When email checkbox selected, default for the public & private email is automatically the account email. Adding an email to the account basics page should save the account basics page & when the email button is selected, the email is automatically pulled & shown to the user.
    - The phone number field automatically formats the number to the correct 000-000-0000 format
    - The location field uses location services to determine the location if the location icon is selected.
    
    Create account basics section of manage account page
    - The user can add a location & the timezone is based on this location.
    - The location can auto be detected by location services by clicking on the location icon.
    
    Create profile section of manage account page

--- app/scripts/account/controllers/privacy.js
+++ app/scripts/account/controllers/privacy.js
@@ -1,10 +1,23 @@
 'use strict';
 angular.module('dmc.account')
-    .controller('PrivacyAccountCtr', [ '$stateParams', '$state', "$scope", function ($stateParams, $state, $scope) {
+    .controller('PrivacyAccountCtr', [ '$stateParams', '$state', "$cookies", "$scope", "location", function ($stateParams, $state, $cookies, $scope, location) {
         $scope.accountId = $stateParams.accountId;
         $scope.page = $state.current.name.split('.')[1];
         $scope.title = pageTitles[$scope.page];
 
+        var userBasics = $cookies.getObject('userBasics');
+        var currentContainer = null;
+        var callback = function(success,data){
+            if(success == true) {
+                $scope.information[currentContainer].location.dataLocation = data;
+                $scope.information[currentContainer].location.value = $scope.information[currentContainer].location.dataLocation.city + ", " + $scope.information[currentContainer].location.dataLocation.region;
+            }
+        };
+
+        $scope.getLocation = function(container){
+            currentContainer = container;
+            location.get(callback);
+        };
 
         var information_ = {
             email : {
@@ -25,6 +38,16 @@ angular.module('dmc.account')
                 value : null,
                 icon : "gps_fixed"
             }
+        };
+
+        $scope.changedCheckbox = function(type, container, item){
+            if(type == "email" && item == true){
+                $scope.information[container].email.value = userBasics.email;
+            }
+        };
+
+        $scope.keyDown = function(type, container, $event){
+            return false;
         };
 
         $scope.information = {
@@ -60,3 +83,83 @@ angular.module('dmc.account')
             }
         };
 }]);
+
+
+angular.module('dmc.account').directive('phoneInput', function($filter, $browser) {
+        return {
+            require: 'ngModel',
+            link: function($scope, $element, $attrs, ngModelCtrl) {
+                var listener = function() {
+                    var value = $element.val().replace(/[^0-9]/g, '');
+                    $element.val($filter('tel')(value, false));
+                };
+
+                // This runs when we update the text field
+                ngModelCtrl.$parsers.push(function(viewValue) {
+                    return viewValue.replace(/[^0-9]/g, '').slice(0,10);
+                });
+
+                // This runs when the model gets updated on the scope directly and keeps our view in sync
+                ngModelCtrl.$render = function() {
+                    $element.val($filter('tel')(ngModelCtrl.$viewValue, false));
+                };
+
+                $element.bind('change', listener);
+                $element.bind('keydown', function(event) {
+                    var key = event.keyCode;
+                    // If the keys include the CTRL, SHIFT, ALT, or META keys, or the arrow keys, do nothing.
+                    // This lets us support copy and paste too
+                    if (key == 91 || (15 < key && key < 19) || (37 <= key && key <= 40)){
+                        return;
+                    }
+                    $browser.defer(listener); // Have to do this or changes don't get picked up properly
+                });
+
+                $element.bind('paste cut', function() {
+                    $browser.defer(listener);
+                });
+            }
+
+        };
+});
+
+angular.module('dmc.account').filter('tel', function () {
+        return function (tel) {
+            if (!tel) { return ''; }
+
+            var value = tel.toString().trim().replace(/^\+/, '');
+
+            if (value.match(/[^0-9]/)) {
+                return tel;
+            }
+
+            var country, city, number;
+
+            switch (value.length) {
+                case 1:
+                case 2:
+                case 3:
+                    city = value;
+                    break;
+
+                default:
+                    city = value.slice(0, 3);
+                    number = value.slice(3);
+            }
+
+            if(number){
+                if(number.length>3){
+                    number = number.slice(0, 3) + '-' + number.slice(3,7);
+                }
+                else{
+                    number = number;
+                }
+
+                return ( city +"-" + number).trim();
+            }
+            else{
+                return city;
+            }
+
+        };
+});
--- app/scripts/account/controllers/profile.js
+++ app/scripts/account/controllers/profile.js
@@ -1,8 +1,62 @@
 'use strict';
 angular.module('dmc.account')
-    .controller('ProfileAccountCtr', [ '$stateParams', '$state', "$scope", function ($stateParams, $state, $scope) {
+    .controller('ProfileAccountCtr', [ '$stateParams', '$state', "$scope", "$cookies", "location", function ($stateParams, $state, $scope, $cookies, location) {
         $scope.accountId = $stateParams.accountId;
         $scope.page = $state.current.name.split('.')[1];
         $scope.title = pageTitles[$scope.page];
 
+        var userBasics = $cookies.getObject('userBasics');
+
+        var callback = function(success,data){
+            if(success == true) {
+                $scope.profile.dataLocation = data;
+                $scope.profile.location = $scope.profile.dataLocation.city + ", " + $scope.profile.dataLocation.region;
+            }
+        };
+
+        $scope.getLocation = function(container){
+            location.get(callback);
+        };
+
+        $scope.profile = {
+            skills : [
+                {
+                    id : 1,
+                    name : 'AngularJS'
+                },{
+                    id : 2,
+                    name : 'Java'
+                },{
+                    id : 3,
+                    name : 'jQuery'
+                },{
+                    id : 4,
+                    name : 'CSS3'
+                },{
+                    id : 5,
+                    name : 'Ruby on Rails'
+                }
+            ],
+            location : (userBasics.location ? userBasics.location : null)
+        };
+
+        $scope.deleteSkill = function(id){
+            for(var s=0;s<$scope.profile.skills.length;s++){
+                if(parseInt($scope.profile.skills[s].id) == parseInt(id)){
+                    $scope.profile.skills.splice(s,1);
+                    break;
+                }
+            }
+        };
+
+        $scope.addSkill = function(name){
+            var max = 0;
+            for(var s=0;s<$scope.profile.skills.length;s++) {
+                if(max < $scope.profile.skills[s].id) max = $scope.profile.skills[s].id;
+            }
+            $scope.profile.skills.push({
+                id : max+1,
+                name : name
+            });
+        };
 }]);
