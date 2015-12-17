164f5b3b504951ec92c4f79a28c5e3dc4c75369f
Wise <200005921@logon.ds.ge.com>
2015-12-17 10:14:17 -0500

    Added home page tests; cleaned up base class

--- selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/HomePageLinksTest.java
+++ selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/HomePageLinksTest.java
@@ -2,52 +2,89 @@ package com.ge.research.vehicleforge.seleniumtests;
 
 import static org.junit.Assert.fail;
 import static org.junit.Assert.assertTrue;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 /**
  * @author Amine Chigani
- * 
+ *
  * Unit tests for navigating through the Home page of the site
- * clicking each link and checking whether the correct page is loaded. 
+ * clicking each link and checking whether the correct page is loaded.
  */
 public class HomePageLinksTest extends BaseTest {
-    
-	@Test
-    public void testHomePageLinks() throws Exception {
 
-        //testForgeLogin();
-        
- 
-        // Click on the Home tab after login.
-        driver.findElement(By.linkText("Home")).click();
-        if (!driver.getCurrentUrl().endsWith("index2.php")) {
-            fail("Doesn't go to the Home page when the Home tab is clicked.");
+
+    @Test
+    public void testDashboard () {
+
+        driver.get(baseUrl);
+
+        WebElement titleElement = null;
+        try {
+            titleElement = driver.findElementByXPath("/html/head/title[text() = 'Digital Manufacturing Commons']");
+
+        } catch (Exception ex) {
+            fail("Title element not found!");
         }
-        
-        
-        // Click on "Projects" link and check whether the correct page is loaded.
-        driver.findElement(By.xpath(".//*[@id='body']/div[1]/div[3]/div/div[2]/div[2]/div/div[2]/a/strong")).click();
-        assertTrue(driver.getCurrentUrl().endsWith("softwaremap/full_list.php"));
-        driver.findElement(By.linkText("Home")).click();
-        
-        // Click on "View Site Activity" link and check whether the correct page is loaded.
-        driver.findElement(By.linkText("View Site Activity")).click();
-        if (!driver.getCurrentUrl().endsWith("site_activity.php")) {
-            fail("Doesn't go to the View Site Activity page.");
+        assertTrue(titleElement != null);
+
+        // Click on dashboard link
+        // /html/body/div[2]/div[1]/a/button
+        WebElement dashboardViewButton = null;
+        try {
+            // This finds: <a href="{{p.href}}" class="home-feature">
+            // Note that the script has not executed
+            dashboardViewButton = driver.findElementByXPath("/html/body/div[2]/div[1]/a[1]/button");
+
+        } catch (Exception ex) {
+            fail("Dashboard element not found!");
         }
-        
-        
-        //Return to Home page and then continue check the following link within homepage.
-        driver.findElement(By.linkText("Home")).click();
-        driver.findElement(By.linkText("All newest projects")).click();
-        assertTrue(driver.getCurrentUrl().endsWith("softwaremap/full_list.php"));
-       
-        
-        
-       // Reurn to Home page and then click on a user.
-        driver.findElement(By.linkText("Home")).click();
-               
-       
-	}
+        assertTrue(dashboardViewButton != null);
+        dashboardViewButton.click();
+        String currentURL = driver.getCurrentUrl();
+        assertTrue (currentURL != null);
+        assertTrue (currentURL.endsWith("dashboard.php"));
+
+    }
+
+    @Test
+    public void testMarketplace () {
+
+        driver.get(baseUrl);
+
+        WebElement titleElement = null;
+        try {
+            titleElement = driver.findElementByXPath("/html/head/title[text() = 'Digital Manufacturing Commons']");
+
+        } catch (Exception ex) {
+            fail("Title element not found!");
+        }
+        assertTrue(titleElement != null);
+
+        // Click on dashboard link
+        // /html/body/div[2]/div[1]/a/button
+        WebElement dashboardViewButton = null;
+        try {
+            // This finds: <a href="{{p.href}}" class="home-feature">
+            // Note that the script has not executed
+            dashboardViewButton = driver.findElementByXPath("/html/body/div[2]/div[2]/a[1]/button");
+
+        } catch (Exception ex) {
+            fail("Dashboard element not found!");
+        }
+        assertTrue(dashboardViewButton != null);
+        dashboardViewButton.click();
+        String currentURL = driver.getCurrentUrl();
+        assertTrue (currentURL != null);
+        assertTrue (currentURL.indexOf("marketplace.php") > 0);
+
+    }
+
+
 }
