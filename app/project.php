1ebcf332bc583c8d3f3a9476cf9f3a44967e0fb8
Hussain Rizvi <hussainr@thecreativeadvantage.com>
2016-01-21 00:00:37 -0500

    Refactoring to allow creation integration of task creation with Rest API

--- app/project.php
+++ app/project.php
@@ -106,6 +106,7 @@
     <script src="scripts/project/controllers/discussions.js"></script>
     <script src="scripts/project/controllers/documents.js"></script>
     <script src="scripts/project/controllers/home.js"></script>
+    <script src="scripts/common/models/task-model.js"></script>
     <script src="scripts/project/controllers/tasks.js"></script>
     <script src="scripts/project/controllers/team.js"></script>
     <script src="scripts/project/controllers/workspace.js"></script>
