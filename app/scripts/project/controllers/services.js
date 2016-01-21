d46650d7a9b3dba8ee0a33dbc50215db039f7686
Denis <klassik911@ukr.net>
2016-01-21 17:58:01 +0200

    Update project home (http://ge-dmc-01.thecreativeadvantage.net/project.php#/2/home) - Section boxes should be wider, therefore closer together. They should be 17 pixels away from each other. To do this, make the services & components boxes wider. - Add a Tasks box that replicates the look of the Tasks box on the dashboard - Add due date column to My Tasks - Switch the location of my tasks & discussions boxes - Get rid of status column for components - Change “Share” to “Invite" - Add services & components to pop up menu. For now, put in placeholder icons - Currently, the pop up menu can be opened by clicking very far to the right of the menu. Eliminate this ability - Set a max length for the overview to 1000 characters - Eliminate the project images. Projects will no longer have images

--- app/scripts/project/controllers/services.js
+++ app/scripts/project/controllers/services.js
@@ -1,10 +1,10 @@
 angular.module('dmc.project')
-.controller('projectServicesCtrl', ['$scope', '$stateParams', 'projectData', function ($scope, $stateParams, projectData) {
+.controller('projectServicesCtrl', ['$scope', '$stateParams', 'projectData','$rootScope','$mdDialog', function ($scope, $stateParams, projectData, $rootScope,$mdDialog) {
 	
 	$scope.projectData = projectData;
 
 	$scope.delete = function(item, index){
 		$scope.projectData.services.data.splice(index, 1);
-	}
-	
+	};
+    $rootScope.$on('$stateChangeStart', $mdDialog.cancel);
 }])
