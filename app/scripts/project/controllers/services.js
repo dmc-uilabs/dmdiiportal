8f220caa3c6df2c8926121738a592d04e3f87625
Denis <klassik911@ukr.net>
2016-02-04 12:17:04 +0200

    Refactoring /project.php#/1/services

--- app/scripts/project/controllers/services.js
+++ app/scripts/project/controllers/services.js
@@ -1,14 +1,29 @@
 angular.module('dmc.project')
-.controller('projectServicesCtrl', ['$scope', '$stateParams', 'projectData','$rootScope','$mdDialog', function ($scope, $stateParams, projectData, $rootScope,$mdDialog) {
-	
-	$scope.projectData = projectData;
-    console.log($scope.projectData);
-    for(var item in $scope.projectData.services.data){
-        $scope.projectData.services.data[item].releaseDate = moment($scope.projectData.services.data[item].releaseDate,"DD/MM/YYYY").format("MM/DD/YYYY");
-    }
+.controller('projectServicesCtrl', ['$scope', '$stateParams', 'ajax', 'dataFactory', 'projectData','$rootScope','$mdDialog', function ($scope, $stateParams, ajax, dataFactory, projectData, $rootScope,$mdDialog) {
 
-	$scope.delete = function(item, index){
-		$scope.projectData.services.data.splice(index, 1);
-	};
-    $rootScope.$on('$stateChangeStart', $mdDialog.cancel);
+        $scope.projectData = projectData;
+
+        // get project services
+        $scope.getServices = function(){
+            ajax.get(dataFactory.getServices($scope.projectData.id),{},function(response){
+                $scope.projectData.services.data = response.data;
+                for(var item in $scope.projectData.services.data){
+                    $scope.projectData.services.data[item].releaseDate = moment($scope.projectData.services.data[item].releaseDate,"DD/MM/YYYY").format("MM/DD/YYYY");
+                }
+                apply();
+            });
+        };
+
+        var apply = function(){
+            if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') $scope.$apply();
+        };
+
+        $scope.getServices();
+
+        $scope.delete = function(item, index){
+            $scope.projectData.services.data.splice(index, 1);
+        };
+
+        $rootScope.$on('$stateChangeStart', $mdDialog.cancel);
+
 }])
