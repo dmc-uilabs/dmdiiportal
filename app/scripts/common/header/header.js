dc5170f0a7be4d187501990bece12b77ef02f176
Hussain Rizvi <hussainr@thecreativeadvantage.com>
2016-02-05 07:49:08 -0500

    Documented user and project discussion being followed Updated temp data object structure in header file

--- app/scripts/common/header/header.js
+++ app/scripts/common/header/header.js
@@ -34,11 +34,31 @@ angular.module('dmc.common.header', ['ngAnimate', 'dmc.model.user'])
                 "companyId": 1,
                 "role": 1,
                 "notifications": {
-                  "total": 2,
+                  "total": 4,
                   "items": [
                     {
-                      "title": "Commented on your discussion",
-                      "link": "/project.php#/2/run-service/3"
+                      "event": "new-contact",
+                      "created_at": "8:00 PM",
+                      "title": "John Smith added you as a contact",
+                      "link": "/profile.php#/1"
+                    },
+                    {
+                      "event": "replied-discussion",
+                      "created_at": "6:00 AM",
+                      "title": "Bill Gates replied to your discussion",
+                      "link": "/individual-discussion.php#/1"
+                    },
+                    {
+                      "event": "new-contact",
+                      "created_at": "6:25 PM",
+                      "title": "Bill Gates added you as a contact",
+                      "link": "/profile.php#/2"
+                    },
+                    {
+                      "event": "replied-discussion",
+                      "created_at": "3:12 PM",
+                      "title": "John Smith replied to your discussion",
+                      "link": "/individual-discussion.php#/1"
                     }
                   ]
                 },
@@ -48,21 +68,48 @@ angular.module('dmc.common.header', ['ngAnimate', 'dmc.model.user'])
                     {
                       "title": "Capacitor Bank For Ldr Mining Co",
                       "serviceId": 3,
-                      "projectId": 2
+                      "projectId": 2,
+                      "currentStatus": {
+                        "percentCompleted": "25",
+                        "startDate": "09/20/2015",
+                        "startTime": "11:15:33"
+                      }
                     },
                     {
                       "title": "Incoming Compartment For Ldr Mining",
                       "serviceId": 1,
-                      "projectId": 4
+                      "projectId": 4,
+                      "currentStatus": {
+                        "percentCompleted": "55",
+                        "startDate": "09/22/2015",
+                        "startTime": "11:15:33"
+                      }
                     }
                   ]
                 },
                 "messages": {
-                  "total": 1,
+                  "total": 3,
                   "items": [
                     {
-                      "title": "Patt has invited you to a challenge",
-                      "link": "/project.php#/preview/5"
+                      "user_name": "John Smith",
+                      "image": "/images/carbone.png",
+                      "text": "How was your meeting with the Bill Gates?",
+                      "link": "/message.php#/1",
+                      "created_at": "8:00 PM"
+                    },
+                    {
+                      "user_name": "John Smith",
+                      "image": "/images/carbone.png",
+                      "text": "How was your meeting with the Bill Gates?",
+                      "link": "/message.php#/1",
+                      "created_at": "10:46 PM"
+                    },
+                    {
+                      "user_name": "John Smith",
+                      "image": "/images/carbone.png",
+                      "text": "How was your meeting with the Bill Gates?",
+                      "link": "/message.php#/1",
+                      "created_at": "1:15 PM"
                     }
                   ]
                 }
