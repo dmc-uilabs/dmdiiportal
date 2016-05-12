bb38deba5909a7a1f46af8fe580a5ae6153941d9
Yan <212417509@logon.ds.ge.com>
2016-05-12 14:39:35 -0400

    remove all real credential from source code

--- selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/TestUtils.java
+++ selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/TestUtils.java
@@ -8,23 +8,9 @@ import java.util.Random;
  * @author Amine Chigani
  */
 public class TestUtils {
-
-    
-    //public static final String BASE_URL = "https://test.projectdmc.org/";
-    //public static final String BASE_URL = "http://ec2-52-37-215-63.us-west-2.compute.amazonaws.com/";
-    //public static final String BASE_URL = "http://52.33.101.137/";
-	public static final String BASE_URL = "http://52.37.215.63/";
-	//public static final String BASE_URL = "https://ben-web.opendmc.org/";
-
 	
     public static final int DEFAULT_IMPLICIT_TIMEOUT_SECONDS = 10;
     public static final boolean CREDENTIAL_GATEWAY_REQUIRED = true;
-/*    public static final String CREDENTIAL_GATEWAY_USER = "dmcuser01@gmail.com";
-    public static final String CREDENTIAL_GATEWAY_PASS = "dmcuser01!";*/
-    public static final String CREDENTIAL_FORGE_USER = "selenium";
-    public static final String CREDENTIAL_FORGE_PASS = "VFseleniumTester";
-    public static final String USER_FULL_NAME = "Selenium Tester";
-    public static final String USER_EMAIL = "dmdi.dmc@ge.com";
     public static final boolean ENABLE_JAVASCRIPT = true;
     
     public static final Random randomGenerator = new Random();
