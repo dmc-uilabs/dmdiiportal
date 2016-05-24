45e28c8823da55fa82f830e1d34ba9d07e1d1b9b
Yan <212417509@logon.ds.ge.com>
2016-05-24 17:40:56 -0400

    add test case for changing account basics

--- selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/MyAccount.java
+++ selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/MyAccount.java
@@ -1,22 +1,33 @@
 package com.ge.research.vehicleforge.seleniumtests;
 
-import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MyAccount extends BaseTest {
 	
 	@Test
 	public void testMyAccount() throws Exception{
-		testDMCLogin();
+		//testDMCLogin();
 		
 		driver.findElement(By.xpath("//div[3]/md-menu/button")).click();
-	    driver.findElement(By.xpath("//md-menu-item/a/span")).click();
-	    assertEquals("Manage Account", driver.getTitle());
-	   /* driver.findElement(By.id("editFirstName")).clear();
-	    driver.findElement(By.id("editFirstName")).sendKeys("Test First Name change1");
-	    driver.findElement(By.xpath("//button[3]")).click();*/
+		
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//md-menu-item/a/span")));
+        element.click();
+	    
+	    
+	    System.out.println("Get current title:" + driver.getTitle());
+	    
+	    driver.findElement(By.id("editFirstName")).clear();
+	    driver.findElement(By.id("editFirstName")).sendKeys("Thomasfixed");
+	    driver.findElement(By.id("input_1")).clear();
+	    driver.findElement(By.id("input_1")).sendKeys("Smithfixed");
+	    
+	    driver.findElement(By.xpath("//button[3]")).click();
 	}
 
 }
