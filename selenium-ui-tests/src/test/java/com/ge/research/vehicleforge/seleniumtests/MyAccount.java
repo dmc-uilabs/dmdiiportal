b182768b12d01d1a50c2a8199c6e395223a20bba
Yan <212417509@logon.ds.ge.com>
2016-05-25 11:53:56 -0400

    add test cases for account privacy

--- selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/MyAccount.java
+++ selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/MyAccount.java
@@ -16,16 +16,20 @@ public class MyAccount extends BaseTest {
 	private String location = "Schenectady";
 	private String timeZone = "(UTC -05:00) America/Atikokan";
 	
-	@Test
-	public void testMyAccount() throws Exception{
-		
+	public void myAccount() throws Exception{
 		driver.findElement(By.xpath("//div[3]/md-menu/button")).click();
 		WebDriverWait wait = new WebDriverWait(driver, 10);
 		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//md-menu-item/a/span")));
         element.click();	    
 	    System.out.println("Get current title:" + driver.getTitle());
-	    assertEquals("Manage Account", driver.getTitle());
-	    
+	    assertEquals("Manage Account", driver.getTitle());	
+	}
+	
+	
+	@Test
+	public void testMyAccountBascis() throws Exception{
+	    myAccount();
+		
 	    //edit firstName, lastName;
 	    driver.findElement(By.id("editFirstName")).clear();
 	    driver.findElement(By.id("editFirstName")).sendKeys(firstName);
@@ -52,6 +56,51 @@ public class MyAccount extends BaseTest {
 	    assertEquals(location, locationChanged);
 	    assertEquals(timeZone, timeZoneChanged);
 	   
+	}
+	
+	
+	@Test
+	public void testMyAccountPrivacy() throws Exception{
+		//navigate to PRIVACY tab
+		driver.findElement(By.xpath("//md-list-item[2]/a/div/div")).click();
+		
+		// uncheck all public information
+	    driver.findElement(By.xpath("//md-checkbox/div")).click();
+	    driver.findElement(By.xpath("//div[2]/md-checkbox/div")).click();
+	    driver.findElement(By.xpath("//div[3]/md-checkbox/div")).click();
+	    
+	    //uncheck all private information
+	    driver.findElement(By.xpath("//md-checkbox/div")).click();
+	    driver.findElement(By.xpath("//div[2]/md-checkbox/div")).click();
+	    driver.findElement(By.xpath("//div[3]/md-checkbox/div")).click();
+	    
+	    //check all public information
+	    driver.findElement(By.xpath("//div[2]/div/div/md-checkbox/div")).click();
+	    driver.findElement(By.xpath("//div[2]/div/div[2]/md-checkbox/div")).click();
+	    driver.findElement(By.xpath("//div[2]/div/div[3]/md-checkbox/div")).click();
+	    
+	    // check all private information
+	    driver.findElement(By.xpath("//div[2]/div/div/md-checkbox/div")).click();
+	    driver.findElement(By.xpath("//div[2]/div/div[2]/md-checkbox/div")).click();
+	    driver.findElement(By.xpath("//div[2]/div/div[3]/md-checkbox/div")).click();
+	    
+	    //save changes
+	    driver.findElement(By.xpath("//button[2]")).click();
+	}
+	
+	
+	@Test
+	public void testMyAccountNotification() throws Exception{
+		//navigate to NOTIFICATIONS tab
+		driver.findElement(By.xpath("//md-list-item[3]/a/div/div")).click();
+		//navigate to EMAIL sub-tab
+		driver.findElement(By.xpath("//md-tab-item[2]/span")).click();
+		assertEquals("EMAIL", driver.findElement(By.xpath("//md-tab-item[2]/span")).getText());
+		//navigate to WEBSITE sub-tab
+	    driver.findElement(By.xpath("//md-tab-item/span")).click();
+	    assertEquals("WEBSITE", driver.findElement(By.xpath("//md-tab-item")).getText());
+		
+		
 	}
 
 }
