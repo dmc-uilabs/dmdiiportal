622f1ab023235be8c554439cd003514b3c882f01
Jerome <jerome.francis@ge.com>
2016-06-20 16:02:11 -0400

    tasks navigation framework in place

--- selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/TasksPageTest.java
+++ selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/TasksPageTest.java
@@ -10,25 +10,25 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class TasksPageTest extends BaseTest {
- 
-  @Test
-  public void testTasksPage() throws Exception {
-		
-		String header = TestUtils.getHeader();  	  
-    driver.get(baseUrl + "/project.php#/5/tasks");
-    driver.findElement(By.xpath("//div/md-icon")).click();
-    driver.findElement(By.xpath("//div/md-icon")).click();
-    driver.findElement(By.cssSelector("th[name=\"Assigned To\"] > div.ng-binding.ng-scope")).click();
-    driver.findElement(By.cssSelector("th[name=\"Assigned To\"] > div.ng-binding.ng-scope")).click();
-    driver.findElement(By.xpath("//th[3]/div/md-icon")).click();
-    driver.findElement(By.xpath("//th[3]/div/md-icon")).click();
-    driver.findElement(By.cssSelector("th[name=\"Priority\"] > div.ng-binding.ng-scope")).click();
-    driver.findElement(By.cssSelector("th[name=\"Priority\"] > div.ng-binding.ng-scope")).click();
-    driver.findElement(By.cssSelector("#select_value_label_1 > span")).click();
-    driver.findElement(By.cssSelector("#select_option_6 > div.md-text.ng-binding")).click();
-    driver.findElement(By.cssSelector("#select_value_label_8 > span")).click();
-    driver.findElement(By.cssSelector("#select_option_14 > div.md-text.ng-binding")).click();
-  }
 
-  
+	@Test
+	public void testTasksPage() throws Exception {
+
+		String header = TestUtils.getHeader();  	  
+		driver.get(baseUrl + "/project.php#/5/tasks");
+		driver.findElement(By.xpath("//th/div")).click();
+		driver.findElement(By.xpath("//div/md-icon")).click();
+		driver.findElement(By.xpath("//th[2]/div")).click();
+		driver.findElement(By.xpath("//th[2]/div")).click();
+		driver.findElement(By.xpath("//th[3]/div/md-icon")).click();
+		driver.findElement(By.xpath("//th[3]/div/md-icon")).click();
+		driver.findElement(By.xpath("//th[4]/div/md-icon")).click();
+		driver.findElement(By.xpath("//th[4]/div/md-icon")).click();
+		driver.findElement(By.linkText("task has been edited for selenium")).click();
+		driver.findElement(By.xpath("//div/div/button")).click();
+		driver.findElement(By.xpath("//div/div/button")).click();
+		driver.findElement(By.xpath("//div/div/button")).click();
+	}
+
+
 }
