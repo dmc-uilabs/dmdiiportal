df634a5d790ecfaebc63e29e0fa621e96c30c2b7
Yan <212417509@logon.ds.ge.com>
2016-06-20 14:15:02 -0400

    rename all test class name with key word *Test

--- selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/MyProfileTest.java
+++ selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/MyProfileTest.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class MyProfile extends BaseTest{
+public class MyProfileTest extends BaseTest{
 	
 	
 	WebDriverWait wait = new WebDriverWait(driver, 20);
