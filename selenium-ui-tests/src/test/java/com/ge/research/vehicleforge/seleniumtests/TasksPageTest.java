3abf14989551ced086d78a1a4891a85c22c63c3d
Jerome <jerome.francis@ge.com>
2016-06-27 13:17:11 -0400

    removing unused imports

--- selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/TasksPageTest.java
+++ selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/TasksPageTest.java
@@ -1,13 +1,9 @@
 package com.ge.research.vehicleforge.seleniumtests;
 
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
+
 import org.junit.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class TasksPageTest extends BaseTest {
 
@@ -15,7 +11,7 @@ public class TasksPageTest extends BaseTest {
 	@Test
 	public void testTasksPage() throws Exception {
 
-		String header = TestUtils.getHeader();  	  
+		 	  
 		driver.get(baseUrl + "/project.php#/5/tasks");
 		driver.findElement(By.xpath("//th/div")).click();
 		driver.findElement(By.xpath("//th/div")).click();
