7e3be574a4ee19a4b9fe17571b7bc3a602f7c645
Jerome <jerome.francis@ge.com>
2016-06-29 10:17:50 -0400

    removing unused import

--- selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/IndividualDiscussionTest.java
+++ selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/IndividualDiscussionTest.java
@@ -12,8 +12,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 
