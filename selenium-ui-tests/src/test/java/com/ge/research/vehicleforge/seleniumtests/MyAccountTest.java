b9b73eea7f770d49045dd19c69a38edf5e43c886
benbeckmann <dmdi.dmc@ge.com>
2016-06-30 20:54:36 +0000

    Fixing removal of "public class MyAccountTest extends BaseTest {".

--- selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/MyAccountTest.java
+++ selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/MyAccountTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+public class MyAccountTest extends BaseTest {
 	private String firstName = "Thomas-changed";
 	private String lastName = "Smith-changed";
 	private String location = "Schenectady";
