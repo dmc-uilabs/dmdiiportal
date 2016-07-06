2c45a640fd3e896316f029b4566401bd6533b55a
Junrong <gloria.yanrr@gmail.com>
2016-07-06 18:06:22 -0400

    fix test case failures for MenuNavigationTest

--- selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/MenuNavigationTest.java
+++ selenium-ui-tests/src/test/java/com/ge/research/vehicleforge/seleniumtests/MenuNavigationTest.java
@@ -4,35 +4,54 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class MenuNavigationTest extends BaseTest {
+	WebDriverWait wait = new WebDriverWait(driver, 30);
 	
 	@Test
 	public void testMenuNavigation() throws Exception{
 		driver.findElement(By.xpath("//a/span")).click();
 	    assertEquals("Dashboard", driver.getTitle());
 	    driver.findElement(By.xpath("//div[2]/div/div/div/div/md-menu/button")).click();
-	    driver.findElement(By.xpath("//md-menu-item/a/span")).click();
+	    Thread.sleep(2000);
+	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//md-menu-item/a/span"))).click();
 	    assertEquals("Marketplace", driver.getTitle());
+	    
+	    Thread.sleep(2000);
 	    driver.findElement(By.xpath("//div[2]/div/div/div/div/md-menu/button")).click();
-	    driver.findElement(By.xpath("//md-menu-item[2]/a/span")).click();
+	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//md-menu-item[2]/a/span"))).click();
 	    assertEquals("Company", driver.getTitle());
+	    
+	    Thread.sleep(2000);
 	    driver.findElement(By.xpath("//div[2]/div/div/div/div/md-menu/button")).click();
-	    driver.findElement(By.xpath("//md-menu-item[3]/a/span")).click();
+	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//md-menu-item[3]/a/span"))).click();
 	    assertEquals("All Favorites", driver.getTitle());
+	    
+	    Thread.sleep(2000);
 	    driver.findElement(By.xpath("//a[2]/span")).click();
 	    assertEquals("Community", driver.getTitle());
+	    
+	    Thread.sleep(2000);
 	    driver.findElement(By.xpath("//div[2]/div/div/div/div/md-menu[2]/button")).click();
-	    driver.findElement(By.xpath("//md-menu-item/a/span")).click();
+	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//md-menu-item/a/span"))).click();
 	    assertEquals("My Projects", driver.getTitle());
+	    
+	    Thread.sleep(2000);
 	    driver.findElement(By.xpath("//div[2]/div/div/div/div/md-menu[2]/button")).click();
-	    driver.findElement(By.xpath("//md-menu-item[2]/a/span")).click();
+	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//md-menu-item[2]/a/span"))).click();
 	    assertEquals("All Projects", driver.getTitle());
-	    driver.findElement(By.xpath("//div[2]/div/div/div/div/md-menu[3]/button")).click();
-	    driver.findElement(By.xpath("//md-menu-item/a/span")).click();
+	    
+	    Thread.sleep(2000);
+	    driver.findElement(By.xpath("//div[2]/div/div/div/div/md-menu[3]/button")).click(); 
+	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//md-menu-item/a/span"))).click();
 	    assertEquals("Member Directory", driver.getTitle());
-	    driver.findElement(By.xpath("//div[2]/div/div/div/div/md-menu[3]/button")).click();
-	    driver.findElement(By.xpath("//md-menu-item[2]/a/span")).click();
+	    
+	    /*driver.findElement(By.xpath("//div[2]/div/div/div/div/md-menu[3]/button")).click();
+	    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//md-menu-item[2]/a/span"))).click();*/
+
 	}
 
 }
