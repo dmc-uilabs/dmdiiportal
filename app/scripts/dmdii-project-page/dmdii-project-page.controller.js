5e97861ce220af6a69c6fddf31da815342e15989
alex maties <alex.maties@uilabs.org>
2016-09-01 17:09:41 -0500

    updating the number formatting to include two digits

--- app/scripts/dmdii-project-page/dmdii-project-page.controller.js
+++ app/scripts/dmdii-project-page/dmdii-project-page.controller.js
@@ -90,4 +90,17 @@ angular.module('dmc.dmdiiProj')
             $scope.getDMDIIProject();
 		}
     ]
-);
+).filter('numberFixedLen', function () {
+        return function (n, len) {
+            var num = parseInt(n, 10);
+            len = parseInt(len, 10);
+            if (isNaN(num) || isNaN(len)) {
+                return n;
+            }
+            num = ''+num;
+            while (num.length < len) {
+                num = '0'+num;
+            }
+            return num;
+        };
+    });
