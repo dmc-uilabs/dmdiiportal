cfa6b08408fdcefe87c8e349b22242dd2ae1cc3b
DMCdevelopment <joe.mazrimas@uilabs.org>
2017-01-12 16:27:44 -0600

    checkbox working correctly in chrome

--- app/scripts/onboarding/controllers/basic-informations.js
+++ app/scripts/onboarding/controllers/basic-informations.js
@@ -11,6 +11,9 @@ angular.module('dmc.onboarding')
 		$scope.ctrl.queryCompanySearch = queryCompanySearch;
 		$scope.ctrl.searchCompanyChange = searchCompanyChange;
 
+		$scope.isDMDIIMember = false;
+		$scope.autocompleteStyle = {'float':'left', 'display': 'none'};
+
         var getAllCompanies = function() {
             ajax.get(dataFactory.companyURL().all, {}, function(response){
                 $scope.ctrl.companies = response.data;
@@ -37,6 +40,24 @@ angular.module('dmc.onboarding')
 			} else {
 				return results;
 			}
+		}
+
+		$scope.$watch('isDMDIIMember', function() {
+		    changeIsDMDIIMember()
+		});
+
+		function changeIsDMDIIMember(){
+		  // company 13 is 'other'
+		  if($scope.info.company != 13){
+		    $scope.info.company = 13;
+				$scope.autocompleteStyle = {'float':'left', 'display': 'none'};
+		  } else {
+				$scope.ctrl.selectedCompany = "";
+				$scope.ctrl.searchCompany = "";
+		    $scope.info.company = null;
+				// $scope.item.name = "";
+		    $scope.autocompleteStyle = {'float':'left'};
+		  }
 		}
 
 		function setCompany(company) {
