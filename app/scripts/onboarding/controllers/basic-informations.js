ceb2829bda2112de3152435ace0fc0a7083f6fc3
DMCdevelopment <joe.mazrimas@uilabs.org>
2017-02-07 09:11:29 -0600

    template reformatted to encourage choosing an organization. Still not re-aligning as would probably be best

--- app/scripts/onboarding/controllers/basic-informations.js
+++ app/scripts/onboarding/controllers/basic-informations.js
@@ -11,8 +11,10 @@ angular.module('dmc.onboarding')
 		$scope.ctrl.queryCompanySearch = queryCompanySearch;
 		$scope.ctrl.searchCompanyChange = searchCompanyChange;
 
-		$scope.isDMDIIMember = false;
-		$scope.autocompleteStyle = {'float':'left', 'display': 'none'};
+		// $scope.isDMDIIMember = false;
+		$scope.isNotDMDIIMember = false;
+		$scope.isNotDMDIIMemberStyle = {'color':'grey'}
+		// $scope.autocompleteStyle = {'float':'left', 'display': 'none'};
 
         var getAllCompanies = function() {
             ajax.get(dataFactory.companyURL().all, {}, function(response){
@@ -42,22 +44,26 @@ angular.module('dmc.onboarding')
 			}
 		}
 
-		$scope.$watch('isDMDIIMember', function() {
+		$scope.$watch('isNotDMDIIMember', function() {
 		    changeIsDMDIIMember()
 		});
 
 		function changeIsDMDIIMember(){
 		  // company 13 is 'other'
-		  if($scope.info.company != 318){
+		  // if($scope.info.company != 318){
+			if($scope.isNotDMDIIMember){
 		    $scope.info.company = 318;
-				$scope.autocompleteStyle = {'float':'left', 'display': 'none'};
+				$scope.isNotDMDIIMemberStyle = {'color':'black'}
+				// $scope.autocompleteStyle = {'float':'left', 'display': 'none'};
 		  } else {
 				$scope.ctrl.selectedCompany = "";
 				$scope.ctrl.searchCompany = "";
 		    $scope.info.company = null;
+				$scope.isNotDMDIIMemberStyle = {'color':'grey'}
 				// $scope.item.name = "";
-		    $scope.autocompleteStyle = {'float':'left'};
+		    // $scope.autocompleteStyle = {'float':'left'};
 		  }
+
 		}
 
 		function setCompany(company) {
