a93f1f82705162333c6417c9e47c579b723506f6
DMCdevelopment <joe.mazrimas@uilabs.org>
2017-04-03 17:13:47 -0500

    created skeleton of recursive release note template

--- app/scripts/release-notes/release-notes-controller.js
+++ app/scripts/release-notes/release-notes-controller.js
@@ -2,6 +2,29 @@
 
 angular.module('dmc.release-notes')
     .controller('release-notes-controller', ['$stateParams', '$state', "$scope", "ajax", function ($stateParams, $state, $scope, ajax) {
-
+      $scope.notes = [
+        {
+          content: "Some text!",
+          notes: [
+            {content: "im one layer deep",
+            notes: [
+              {content: "im TWO layers deep!"}
+            ]
+            },
+            {content: "im one layer deep",
+            notes: [
+              {content: "two",
+              notes: [
+                {content: "threee!!!!!!!!!"}
+              ]
+              }
+            ]
+            }
+          ]
+        },
+        {
+          content: "Some other text!"
+        }
+      ]
     }]
 );
