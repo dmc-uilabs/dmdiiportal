8c1b9c831091091e3de42c639bc8f4513836cdfd
Clay Taylor <clay.taylor@e-gineering.com>
2017-05-03 18:20:54 -0400

    added fix for username on service run

--- app/scripts/project/project.js
+++ app/scripts/project/project.js
@@ -219,7 +219,9 @@ angular.module('dmc.project', [
                 templateUrl: 'templates/project/pages/run-history.html',
                 resolve: {
                     runHistory: ['serviceModel', '$stateParams', function (serviceModel, $stateParams) {
-                        return serviceModel.get_service_run_history($stateParams.ServiceId);
+                        return serviceModel.get_service_run_history($stateParams.ServiceId, {}, function (response) {
+                            return response;
+                        });
                     }],
                     serviceData: ['serviceModel', '$stateParams', function (serviceModel, $stateParams) {
                         return serviceModel.get_service($stateParams.ServiceId);
@@ -1045,30 +1047,29 @@ angular.module('dmc.project', [
             };
 
             this.get_service_run_history = function(id, params, callback){
-                return ajax.get(dataFactory.services(id).get_run_history,
-                    (params)? params : {
-                        _sort: 'id',
-                        _order: 'DESC',
-                        status_ne : 'running'
-                    },
-                    function(response){
-                        var history = response.data;
-                        for(var i in history){
-                            history[i].runTime = calcRunTime(history[i]);
-                            history[i].date = moment(new Date(history[i].startDate+' '+history[i].startTime)).format('MM/DD/YYYY hh:mm A');
-                            ajax.get(dataFactory.getUserName(history[i].runBy), {}, function (response) {
-                                history[i].runByUserName = response.data.displayName;
-                            }, function (response) {
-                                toastModel.showToast("error", "Ajax failed: getUserName");
+                
+                return $http.get(dataFactory.services(id).get_run_history, (params)? params : {
+                    _sort: 'id',
+                    _order: 'DESC',
+                    status_ne : 'running'
+                }, {}).then(function(response){
+                    var history = response.data;
+                    
+                    for (var i = 0; i < history.length; i++) {
+                        history[i].runTime = calcRunTime(history[i]);
+                        history[i].date = moment(new Date(history[i].startDate+' '+history[i].startTime)).format('MM/DD/YYYY hh:mm A');
+                        var userName = function (index) {
+                            $http.get(dataFactory.getUserName(history[index].runBy)).then(function (response) {
+                                var h = history[index];
+                                h.runByUserName = response.data.displayName;
                             });
-                        }
-                        if(callback){
-                            callback(history);
-                        }else{
-                            return history;
-                        }
+                        };
+                        userName(i);
                     }
-                )
+                    
+                    return history;
+                    
+                });
             };
 
 
