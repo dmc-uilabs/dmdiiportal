27ae549276a90bcf288f976d7c09138db1d847e1
DMCdevelopment <joe.mazrimas@uilabs.org>
2017-06-22 11:32:56 -0500

    calendar fixed at larger width, list wrapping correctly

--- app/scripts/dmdii-events/dmdii-events.js
+++ app/scripts/dmdii-events/dmdii-events.js
@@ -26,27 +26,29 @@ angular.module('dmc.dmdiiEvents', [
     'dmc.component.horizontalmenu',
     'dmc.widgets.tasks',
     'dmc.widgets.tabs',
-    'dmc.recent-news'
+    'dmc.recent-news',
+    'tien.clndr'
 ])
 .config(function($stateProvider, $urlRouterProvider, $httpProvider){
     $stateProvider.state('dmdii_events', {
         url: '/dmdii_events?status?rootNumber?callNumber?statusId?focusId?thrustId',
-        templateUrl: 'templates/dmdii-projects/dmdii-projects.html',
-        controller: 'DMCDmdiiProjectsController',
+        templateUrl: 'templates/dmdii-events/dmdii-events.html',
+        controller: 'DMCDmdiiEventsController',
         resolve: {
             is_search: function() {
                 return false;
             }
         }
-    }).state('dmdii_events_search', {
-		url: '/dmdii_events/search?status?rootNumber?callNumber?statusId?focusId?thrustId',
-        templateUrl: 'templates/dmdii-projects/dmdii-projects.html',
-        controller: 'DMCDmdiiProjectsController',
-        resolve: {
-            is_search: function() {
-                return true;
-            }
-        }
-    });
+    })
+    // .state('dmdii_events_search', {
+		// url: '/dmdii_events/search?status?rootNumber?callNumber?statusId?focusId?thrustId',
+    //     templateUrl: 'templates/dmdii-projects/dmdii-projects.html',
+    //     controller: 'DMCDmdiiProjectsController',
+    //     resolve: {
+    //         is_search: function() {
+    //             return true;
+    //         }
+    //     }
+    // });
     $urlRouterProvider.otherwise('/dmdii_events');
 });
