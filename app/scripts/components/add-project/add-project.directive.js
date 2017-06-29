f660604857ca90e99de283a26fa19b37fdf05c69
aca089 <atuladhikari2016@u.northwestern.edu>
2017-06-29 09:59:00 -0500

    changes to workspace creation form template

--- app/scripts/components/add-project/add-project.directive.js
+++ app/scripts/components/add-project/add-project.directive.js
@@ -4,6 +4,7 @@ angular.module('dmc.add_project.directive', [
     'dmc.ajax',
     'dmc.data',
     'dmc.model.project',
+    'dmc.add_members',
     'dmc.model.member',
     'ngMdIcons',
     'dmc.model.question-toast-model',
@@ -100,6 +101,7 @@ angular.module('dmc.add_project.directive', [
                 }
             };
 
+
             $scope.disableEnable = function(number,val){
                 var v = (val ? false : true);
                 switch(number){
@@ -147,17 +149,19 @@ angular.module('dmc.add_project.directive', [
                 goToTab : '=',
                 disableEnableTab : '=',
                 documents: '=',
+                submitProjectDetails:'=',
                 // user input is save on this object
                 projectDetails: '=',
                 isUpdate: '='
             },
-            controller: function ($scope) {
+            controller: ["$scope","$mdDialog", function ($scope,$mdDialog) {
                 if($scope.projectDetails){
                     if($scope.projectDetails.dueDate){
                         $scope.projectDetails.dueDate = new Date($scope.projectDetails.currentDueDate);
                         apply();
                     }
                 }
+
                 $scope.$watch('form.$valid', function(current, old){
                     $scope.disableEnableTab(2,current);
                 });
@@ -176,6 +180,20 @@ angular.module('dmc.add_project.directive', [
                     }
                     $scope.isChanges = Object.keys(changedValues).length == 0 ? false : true;
                 };
+
+                $scope.addMembersWp = function(ev){
+                  console.log("function add Members called")
+                  $(window).scrollTop(0);
+                  $mdDialog.show({
+                      controller: 'AddMembersController',
+                      // templateUrl: 'templates/add_members/add-members.html',
+                      templateUrl:'templates/components/add-project/ap-tab-two.html',
+                      parent: angular.element(document.body),
+                      targetEvent: ev,
+                      fullscreen:true,
+                      clickOutsideToClose:true
+                  })
+                }
 
                 //$scope.cancelChanges = function(){
                 //    if($scope.isUpdate && $scope.isChanges) {
@@ -220,7 +238,7 @@ angular.module('dmc.add_project.directive', [
                 function apply() {
                     if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') $scope.$apply();
                 }
-            }
+            }]
         }
     })
     .directive('apTabTwo', function (DMCMemberModel) {
@@ -324,6 +342,7 @@ angular.module('dmc.add_project.directive', [
                     isInvite();
                 };
 
+
                 $scope.addToInvitation = function(item){
                     var found = false;
                     for(var i in $scope.invitees){
@@ -390,34 +409,36 @@ angular.module('dmc.add_project.directive', [
                 $scope.outputs = new Array($scope.totalOutputs);
             }
         }
-    }).directive('dmcSelectedInvitees', function () {
-        return {
-            restrict: 'A',
-            templateUrl: 'templates/components/add-project/selected-invitees-tpl.html',
-            scope:{
-                invitees: '='
-            },
-            controller: function ($scope) {
-
-                $scope.removeInvite = function(item){
-                    for(var i in $scope.invitees){
-                        if($scope.invitees[i].id === item.id){
-                            $scope.invitees.splice(i,1);
-                            if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') $scope.$apply();
-                            break;
-                        }
-                    }
-                };
-
-
-
-                $scope.clear = function(){
-                    $scope.invitees = [];
-                    if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') $scope.$apply();
-                };
-            }
-        }
-    }).directive('dmcMemberCard', function () {
+    })
+    // .directive('dmcSelectedInvitees', function () {
+    //     return {
+    //         restrict: 'A',
+    //         templateUrl: 'templates/components/add-project/selected-invitees-tpl.html',
+    //         scope:{
+    //             invitees: '='
+    //         },
+    //         controller: function ($scope) {
+    //
+    //             $scope.removeInvite = function(item){
+    //                 for(var i in $scope.invitees){
+    //                     if($scope.invitees[i].id === item.id){
+    //                         $scope.invitees.splice(i,1);
+    //                         if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') $scope.$apply();
+    //                         break;
+    //                     }
+    //                 }
+    //             };
+    //
+    //
+    //
+    //             $scope.clear = function(){
+    //                 $scope.invitees = [];
+    //                 if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') $scope.$apply();
+    //             };
+    //         }
+    //     }
+    // })
+    .directive('dmcMemberCard', function () {
         return {
             restrict: 'A',
             templateUrl: 'templates/components/add-project/member-card-tpl.html',
@@ -714,4 +735,217 @@ angular.module('dmc.add_project.directive', [
                 };
             }
         }
-    });
+    })
+    .controller('AddMembersController', [
+        '$scope',
+        'DMCMemberModel',
+        'projectModel',
+        '$stateParams',
+        'toastModel',
+        'ajax',
+        'dataFactory',
+        '$cookieStore',
+        function ($scope,
+                  DMCMemberModel,
+                  projectModel,
+                  $stateParams,
+                  toastModel,
+                  ajax,
+                  dataFactory,
+                  $cookieStore) {
+            DMCMemberModel.getMembers().then(
+                function(data){
+                    $scope.foundMembers = data;
+                    isInvite();
+                },
+                function(data){}
+            );
+
+
+
+            $scope.compare = [];
+            $scope.invitees = [];
+            $scope.favorites = [];
+            $scope.showFavoritesFlag = false;
+
+            function isInvite(){
+                for(var i in $scope.foundMembers){
+                    var found = false;
+                    for(var j in $scope.invitees){
+                        if(($scope.invitees[j].profileId && $scope.foundMembers[i].id == $scope.invitees[j].profileId) || (!$scope.invitees[j].profileId && $scope.foundMembers[i].id == $scope.invitees[j].id)){
+                            found = true;
+                            break;
+                        }
+                    }
+                    $scope.foundMembers[i].isInvite = found;
+                }
+            }
+
+            $scope.selectedTab = 0;
+            var askLeave = true;
+            $scope.$on('$locationChangeStart', function (event, next, current) {
+                if(askLeave) {
+                    event.preventDefault();
+                    questionToastModel.show({
+                        question: 'Are you sure you want to leave this page?',
+                        buttons: {
+                            ok: function () {
+                                askLeave = false;
+                                $(window).unbind('beforeunload');
+                                window.location = next;
+                            },
+                            cancel: function () {
+                            }
+                        }
+                    }, event);
+                }
+            });
+
+            $(window).unbind('beforeunload');
+            $(window).bind('beforeunload', function(){
+                return '';
+            });
+
+            $scope.$watchCollection('invitees',function(newArray,oldArray){
+                isInvite();
+            });
+
+            var currentMembers = [];
+            $scope.getMembers = function () {
+                ajax.get(dataFactory.projectMembers($stateParams.projectId), {}, function (response) {
+                    var profileIds = $.map(response.data, function (x) {
+                        return x.profileId;
+                    });
+                    currentMembers = $.map(response.data, function (x) {
+                        return {
+                            id : x.id,
+                            profileId : x.profileId
+                        };
+                    });
+                    $scope.invitees = response.data;
+                    if(profileIds.length > 0) {
+                        ajax.get(dataFactory.profiles().all, {
+                            id: profileIds
+                        }, function (res) {
+                            $scope.invitees = res.data;
+                            apply();
+                        });
+                    }
+                });
+            };
+
+            $scope.getMembers();
+
+            $scope.$watchCollection('compare',function(newArray,oldArray){
+                for(var i in $scope.foundMembers){
+                    var found = false;
+                    for(var j in newArray){
+                        if($scope.foundMembers[i].id == newArray[j].id){
+                            found = true;
+                            break;
+                        }
+                    }
+                    $scope.foundMembers[i].isCompare = found;
+                }
+            });
+
+            $scope.$watchCollection('favorite',function(newArray,oldArray){
+                for(var i in $scope.foundMembers){
+                    var found = false;
+                    for(var j in newArray){
+                        if($scope.foundMembers[i].id == newArray[j].id){
+                            found = true;
+                            break;
+                        }
+                    }
+                    $scope.foundMembers[i].isCompare = found;
+                }
+            });
+
+            $scope.compareMember = function(item){
+                var found = false;
+                for(var i in $scope.compare){
+                    if($scope.compare[i].id === item.id) {
+                        $scope.compare.splice(i, 1);
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found) $scope.compare.push(item);
+                apply();
+            };
+
+            function apply(){
+                if ($scope.$root.$$phase != '$apply' && $scope.$root.$$phase != '$digest') $scope.$apply();
+            }
+
+
+            $scope.searchMembers = function(){
+                ajax.get(dataFactory.getUserList(), {
+                    page: 0,
+                    pageSize: 100,
+                    displayName: $scope.searchModel
+                }, function(response) {
+                    $scope.foundMembers = response.data.content;
+                    isInvite();
+                })
+            };
+
+            $scope.NextTab = function(){
+                $scope.selectedTab = 1;
+            }
+
+            $scope.BackTab = function(){
+                $scope.selectedTab = 0;
+            }
+
+            $scope.addToInvitation = function(item){
+                var found = false;
+                for(var i in $scope.invitees){
+                    if($scope.invitees[i].id === item.id) {
+                        $scope.invitees.splice(i, 1);
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found) $scope.invitees.push(item);
+                apply();
+            };
+
+            $scope.addToFavorite = function(item){
+                var found = false;
+                for(var i in $scope.favorites){
+                    if($scope.favorites[i].id === item.id) {
+                        $scope.favorites.splice(i, 1);
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found) $scope.favorites.push(item);
+                apply();
+            };
+
+            // $scope.resetMembers = function(){
+            //     $scope.foundMembers = $scope.allMembers;
+            //     $scope.searchModel = '';
+            //     isInvite();
+            // };
+            //
+
+            $scope.showFavorites = function(){
+                $scope.showFavoritesFlag = !$scope.showFavoritesFlag;
+            };
+
+            $scope.exit = function(listInvitees){
+              console.log(listInvitees);
+            }
+
+            $scope.send = function(){
+                projectModel.add_members_to_project($scope.invitees,currentMembers, function(){
+                    $cookieStore.put('toast', 'Invitations Sent');
+                    $(window).unbind('beforeunload');
+                    $scope.$on('$locationChangeStart', function (event, next, current) {});
+                    document.location.href = 'project.php#/'+$stateParams.projectId+'/home';
+                })
+            }
+        }])
