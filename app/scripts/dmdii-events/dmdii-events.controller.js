70637b8d31ae527cef865f0587b429d745622f3f
DMCdevelopment <joe.mazrimas@uilabs.org>
2017-07-14 11:00:50 -0500

    update paginated call to dmdiiprojects

--- app/scripts/dmdii-events/dmdii-events.controller.js
+++ app/scripts/dmdii-events/dmdii-events.controller.js
@@ -111,7 +111,7 @@ angular.module('dmc.dmdiiEvents')
 
             $scope.getEvents = function(){
                 ajax.get(dataFactory.dmdiiMemberEventUrl().get, {limit: 1000}, addMemEvents);
-                ajax.get(dataFactory.dmdiiProjectEventUrl().get, {limit: 1000}, addProjEvents);
+                ajax.get(dataFactory.dmdiiProjectEventUrl().get, {page: 0, pageSize: 100}, addProjEvents);
                 ajax.get(dataFactory.getDMDIIProject().all, {limit: 1000}, addDMDIIEvents);
             };
 
