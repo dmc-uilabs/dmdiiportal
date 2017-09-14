57869f1b4238ea3d7643f65ea4526b81e01aded9
DMCdevelopment <joe.mazrimas@uilabs.org>
2017-09-14 13:35:22 -0500

    add quickdoc controller to dmdii members

--- app/scripts/member-directory/member-directory.js
+++ app/scripts/member-directory/member-directory.js
@@ -51,4 +51,12 @@ angular.module('dmc.members', [
         }
     });
     $urlRouterProvider.otherwise('/member_directory');
-});
+})
+.controller('QuickDocController',
+	['$scope', '$rootScope', '$mdDialog', 'doc',
+	function ($scope, $rootScope, $mdDialog, doc) {
+        $scope.doc = doc;
+		$scope.cancel = function(){
+            $mdDialog.hide();
+		}
+}]);
